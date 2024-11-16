e584e7bdc6f94fcebe77484d97dc76eaef442d4c
Garaisourav12 <garaisourav12@gmail.com>
2024-11-16 14:21:35 +0530

    oops

--- js/oops/opps2.js
+++ js/oops/opps2.js
@@ -1,33 +1,47 @@
 class Person {
-	name;
-	constructor(name) {
-		this.name = name;
+	#name;
+	_age;
+	constructor(name, age) {
+		this.#name = name;
+		this._age = age;
+	}
+
+	getName() {
+		return this.#name;
+	}
+
+	setName(name) {
+		this.#name = name;
 	}
 
 	walk() {
-		console.log(this.name, "Walking");
+		console.log(this.#name, "Walking");
 	}
 
 	speak() {
-		console.log(this.name, "Speaking");
+		console.log(this.#name, "Speaking");
 	}
 }
 
 // Create instance first
-const p1 = new Person("Shubham");
+const p1 = new Person("Shubham", 22);
 p1.walk();
-const p2 = new Person("Deepak");
+const p2 = new Person("Deepak", 23);
 p2.speak();
 
 class Singer extends Person {
 	instrument;
-	constructor(name, instrument) {
-		super(name);
+	constructor(name, age, instrument) {
+		super(name, age);
 		this.instrument = instrument;
 	}
 
 	sing() {
-		console.log(this.name, "singing");
+		console.log(this.getName(), "singing");
+	}
+
+	tellAge() {
+		console.log("age of", this.getName(), "is", this._age);
 	}
 }
 
@@ -44,7 +58,7 @@ console.log(s1 instanceof Person);
 // Polymorphism
 class Racer extends Person {
 	walk() {
-		console.log(this.name, "walking very fast");
+		console.log(this.getName(), "walking very fast");
 	}
 }
 
@@ -52,3 +66,11 @@ const r1 = new Racer("Arun Sir");
 
 r1.walk();
 r1.speak();
+console.log(s1.instrument);
+// s1.#name = "Yogesh";
+s1.instrument = "Piyano";
+console.log(s1.instrument);
+console.log(p1.getName());
+p1.setName("Suresh");
+console.log(p1.getName());
+console.log(p1._age); // Dont access protected property here
